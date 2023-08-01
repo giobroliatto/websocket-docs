9175e962680c11f60dfe8f143b9d423a3b418c78
giobroliatto <giovanibroliatto@gmail.com>
2023-07-31 21:15:40 -0300

    feat: connection io created

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const port = process.env.port || 3000;
@@ -9,5 +11,12 @@ const currentRoute = url.fileURLToPath(import.meta.url);
 const publicDirectory = path.join(currentRoute, "../..", "public");
 app.use(express.static(publicDirectory));
 
-app.listen(port, () => console.log(`servidor escutando na porta ${port}`));
+const httpServer = http.createServer(app);
 
+httpServer.listen(port, () => console.log(`servidor escutando na porta ${port}`));
+
+const io = new Server(httpServer);
+
+io.on("connection", () => {
+  console.log("cliente se conectou")
+});
